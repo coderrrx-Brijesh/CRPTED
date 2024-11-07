88456a3fbf15750f726abe6cea3d6006186ee09a
Avan14 <145996250+Avan14@users.noreply.github.com>
2024-11-07 18:21:56 +0530

    added comments to backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
+// cors for testing backend and front end together
 const cors = require('cors');
+// backend uses express as server
 const express = require('express');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// all endpoint swill be handled by mainrouter
 const mainRouter = require('./Routes/Route');
-app.use("/api/v1", mainRouter);
+app.use("/app/V1", mainRouter);
 
 app.listen(3000, () => {
     console.log("Listening at port 3000 :)");
