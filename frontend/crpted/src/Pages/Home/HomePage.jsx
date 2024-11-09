0662e67f5e1e66c61a0cb701dd677982f295ee75
Brijesh Singh <coderrrx@gmail.com>
2024-11-10 00:18:53 +0530

    HomePage_Update_1

--- frontend/crpted/src/Pages/Home/HomePage.jsx
+++ frontend/crpted/src/Pages/Home/HomePage.jsx
@@ -1,12 +1,27 @@
-import { Button } from "../../components/ui/button"
+import React from "react";
+import { Button } from "../../components/ui/button";
+import homePageBanner from "../../assets/hero-banner.png";
+import { TypedComponent } from "./PageComponents/Typed"; // imported typed component
+import "./HomePage.css";
 
-export const HomePage= ()=>{
+export const HomePage = () => {
     console.log("home");
     return (
-        <div>
-            <Button>
-                home
-            </Button>
+        <div className="home-page-container">
+            <div className="left-container">
+                <h1 className="text-7xl font-bold text-white leading-snug">
+                    Buy & Sell Digital Assets In The Cryptex
+                </h1>
+                <p className="text-gray-400 text-lg">
+                    Coin Cryptex is the <TypedComponent className="features" /> <br/> way to buy & sell crypto asset exchange.
+                </p>
+                <Button className="bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700">
+                    Get Started Now
+                </Button>
+            </div>
+            <div className="right-container">
+                <img src={homePageBanner} className="home-page-banner" alt="hero-banner" />
+            </div>
         </div>
-    )
-}
+    );
+};
