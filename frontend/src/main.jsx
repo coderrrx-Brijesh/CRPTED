5a2c77c4e743d9ca3dab80de712d41e3ca5ec00d
Brijesh Singh <coderrrx@gmail.com>
2024-12-15 02:25:57 +0530

    ui updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { CryptoContextProvider } from "./Context/CryptoContext";
 import { LogedinProvider } from "./Context/LogedinContext";
-
+import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
   <CryptoContextProvider>
     <LogedinProvider>
+    <Toaster /> 
       <App />
     </LogedinProvider>
     </CryptoContextProvider>
