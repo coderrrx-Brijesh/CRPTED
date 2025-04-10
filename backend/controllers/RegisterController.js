e414618ee20786eef7848f901d6a05582c972745
Brijesh Singh <coderrrx@gmail.com>
2025-04-10 17:26:10 +0530

    fixed git builds

--- backend/controllers/RegisterController.js
+++ backend/controllers/RegisterController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { SignUpSchemaValidator } = require("../validations/UserValidator");
 const saltRounds = 10;
 
